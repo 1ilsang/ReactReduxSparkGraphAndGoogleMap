9ebf92d9913231ab54c5b5919213a624d9e1209a
Jackson Lee <1ilsang@naver.com>
2018-01-17 16:08:25 +0900

    push

---
+++ README.md
@@ -0,0 +1,10 @@
+# Redux로 만든 도시날씨검색 서비스
+
+구글맵, Sparkline 사용.
+
+npm install
+npm start
+
+(https://www.udemy.com/react-redux/)
+"react-google-maps": "4.7.1",
+
---
+++ src/containers/weather_list.js
@@ -0,0 +1,49 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+
+import Chart from '../components/chart';
+import GoogleMap from '../components/google_map';
+
+class WeatherList extends Component {
+    renderWeather(cityData){
+        const name = cityData.city.name;
+        const temps = _.map(cityData.list.map(weather => weather.main.temp), (temp)=> temp-273.15);
+        const pressure = cityData.list.map(weather => weather.main.pressure);
+        const humidities = cityData.list.map(weather => weather.main.humidity);
+        const { lon, lat } = cityData.city.coord;
+
+        return (
+            <tr key={name}>
+                <td><GoogleMap lon={lon} lat={lat} /></td>
+                <td><Chart data={temps} color="orange" units="°C" /></td>
+                <td><Chart data={pressure} color="green" units="nPa" /></td>
+                <td><Chart data={humidities} color="black" units="%" /></td>
+            </tr>
+        );
+    }
+    render() {
+        return (
+            <div>
+                <table className="table table-hover">
+                    <thead>
+                        <tr>
+                            <th>City</th>
+                            <th>Temperature (K)</th>
+                            <th>Pressure (hPa)</th>
+                            <th>Humidity (%)</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {this.props.weather.map(this.renderWeather)}
+                    </tbody>
+                </table>
+            </div>
+        );
+    }
+}
+
+function mapStateToProps({ weather }) {
+    return { weather };
+}
+
+export default connect(mapStateToProps)(WeatherList);
